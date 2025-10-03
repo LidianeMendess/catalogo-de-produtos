3ba1848187c3ce8fafcd13583cd2c90303b6f72b
Lidiane Mendes <lidyaraujo64@gmail.com>
2025-10-03 20:56:22 -0300

    refactor: ajustar DTOs de request e response de produto

--- src/main/java/br/com/cdb/catalogodeprodutos/adapter/input/request/ProdutoRequest.java
+++ src/main/java/br/com/cdb/catalogodeprodutos/adapter/input/request/ProdutoRequest.java
@@ -1,6 +1,7 @@
 package br.com.cdb.catalogodeprodutos.adapter.input.request;
 
 import br.com.cdb.catalogodeprodutos.core.domain.model.Categoria;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +14,29 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProdutoRequest {
+
+    @Schema(description = "Identificador único do produto", example = "1")
     private Integer id;
+
+    @Schema(description = "Código SKU do produto", example = "SKU123")
     private String sku;
+
+    @Schema(description= "Nome do produto", example = "Ração Premium")
     private String nome;
+
+    @Schema(description ="Descrição do produto", example = "Ração Premium para gatos castrados.")
     private String descricao;
+
+    @Schema(description = "Preço unitário", example = "59.90")
     private BigDecimal preco;
+
+    @Schema(description = "Quantidade em estoque", example = "50")
     private Integer quantidade;
+
+    @Schema(description = "Indica se está ativo", example = "true")
     private Boolean ativo;
+
+    @Schema(description = "Categoria", example = "GATO", allowableValues = { "CAO", "GATO", "PASSARO", "PEIXE", "OUTROS"})
     private Categoria categoria;
 }
 
